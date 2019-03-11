a029cbf3a58ba119c5a54a4d954df6487b41b444
michalnov <michalnov3@gmail.com>
2019-03-11 18:24:17 +0100

    ..

--- bin/core/core.go
+++ bin/core/core.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/michalnov/psychology/bin/core/structures"
 
+	//need
 	_ "github.com/go-sql-driver/mysql" //needed
 )
 
@@ -205,3 +206,22 @@ func readTest(name string) (structures.Test, error) {
 	}
 	return out, nil
 }
+
+/*
+func Activationmail(email string, tokenn string, mailer str.Mail) {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "martinhercka1@gmail.com")
+	m.SetHeader("To", email)
+	m.SetHeader("Subject", "Hello")
+	m.SetBody("text/html", mailer.Host+mailer.Port+"/auth/activate?token="+tokenn)
+
+	if mailer.Username == "" || mailer.Password == "" {
+		fmt.Println(mailer.Host + mailer.Port + "/auth/activate?token=" + tokenn)
+		return
+	}
+	d := gomail.NewDialer("smtp.gmail.com", 587, mailer.Username, mailer.Password)
+
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+}
+*/
